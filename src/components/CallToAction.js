7f2519098ab2ffd4271bb510f151875f078f4963
Gawain Bracy II <bracy.gawain2014@gmail.com>
2021-12-02 14:07:32 -0500

    FAQ Updates
    Updated FAQ questions and Call to Action section.

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -10,8 +10,8 @@ const CallToAction = () => {
                 <div className="cta-one__circle-2"></div>
                 <div className="cta-one__circle-3"></div>
                 <div className="cta-one__content text-center">
-                    <h3>Try Our App Today &amp; <br/> Experience Endless Possibilities</h3>
-                    <p>brands get started with a free 1 month trial</p>
+                    <h3>Try The Creative Platform Today &amp; <br/> Experience Endless Possibilities</h3>
+                    <p>Brands get started with a free 1 month trial</p>
                     <a href="#" className="thm-btn cta-one__btn">Coming Soon</a>
                 </div>
             </Container>
